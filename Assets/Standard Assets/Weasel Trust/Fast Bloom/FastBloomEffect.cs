2b041731cb903be9dbb1bf5f9313e288aae7a5fa
Kirill Nadezhdin <cyrilltoboe@gmail.com>
2017-10-25 19:27:58 +0300

    Downsample display

--- Assets/Standard Assets/Weasel Trust/Fast Bloom/FastBloomEffect.cs
+++ Assets/Standard Assets/Weasel Trust/Fast Bloom/FastBloomEffect.cs
@@ -84,14 +84,14 @@ namespace WeaselTrust
             _downsampleMaterial.SetVector("_LuminanceConst", luminanceConst);
             Blit(_mainRenderTexture, _downsampledBrightpassTexture, _downsampleMaterial);
 
-            Blit(_downsampledBrightpassTexture, _preBloomTexture, _brightpassMaterial);
+            //Blit(_downsampledBrightpassTexture, _preBloomTexture, _brightpassMaterial);
 
-            _blurMaterial.SetVector("_SpreadDirection", new Vector4(1f, 0f, 0f, 0f));
-            Blit(_preBloomTexture, _horizontalBlurTexture, _blurMaterial);
+            //_blurMaterial.SetVector("_SpreadDirection", new Vector4(1f, 0f, 0f, 0f));
+            //Blit(_preBloomTexture, _horizontalBlurTexture, _blurMaterial);
 
-            _verticalBlurGammaCorrectionMaterial.SetFloat("_BloomIntencity", bloomIntensity);
-            _verticalBlurGammaCorrectionMaterial.SetPass(0);
-            Blit(_horizontalBlurTexture, _verticalBlurGammaCorrectedTexture, _verticalBlurGammaCorrectionMaterial);
+            //_verticalBlurGammaCorrectionMaterial.SetFloat("_BloomIntencity", bloomIntensity);
+            //_verticalBlurGammaCorrectionMaterial.SetPass(0);
+            //Blit(_horizontalBlurTexture, _verticalBlurGammaCorrectedTexture, _verticalBlurGammaCorrectionMaterial);
         }
 
         private void OnRenderObject()
@@ -99,11 +99,11 @@ namespace WeaselTrust
             int instanceId = Camera.current.GetInstanceID();
             if (instanceId == this._mainCamera.GetInstanceID())
             {
-                _composeMaterial.SetPass(0);
-                Graphics.DrawMeshNow(_fullscreenQuadMesh, Matrix4x4.identity);
-
-                //_displayMainTextureMaterial.SetPass(0);
+                //_composeMaterial.SetPass(0);
                 //Graphics.DrawMeshNow(_fullscreenQuadMesh, Matrix4x4.identity);
+
+                _displayMainTextureMaterial.SetPass(0);
+                Graphics.DrawMeshNow(_fullscreenQuadMesh, Matrix4x4.identity);
             }
             else
             {
@@ -180,7 +180,7 @@ namespace WeaselTrust
             _composeMaterial.SetTexture("_BloomTex", _verticalBlurGammaCorrectedTexture);
 
             //===============
-            _displayMainTextureMaterial.SetTexture("_MainTex", _verticalBlurGammaCorrectedTexture);
+            _displayMainTextureMaterial.SetTexture("_MainTex", _downsampledBrightpassTexture);
             _upscaleBloomMaterial.SetTexture("_BloomTex", _verticalBlurGammaCorrectedTexture);
             //===============
 

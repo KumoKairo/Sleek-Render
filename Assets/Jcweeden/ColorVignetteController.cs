ca4c170b1c3ecd56e608825aff48e89156b51d35
Kirill Nadezhdin <cyrilltoboe@gmail.com>
2018-03-05 10:56:40 +0300

    Random stuff for Jcweeden (to be deleted after)

---
+++ Assets/Jcweeden/ColorVignetteController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class ColorVignetteController : MonoBehaviour 
+{
+	public float vignetteBeginRadius = 0.166f;
+	public float vignetteExpandRadius = 1.34f;
+	public Color vignetteColor = Color.black;
+
+	public Material targetMaterial;
+
+	private static class Uniforms
+	{
+		public static readonly int _VignetteShape = Shader.PropertyToID("_VignetteShape");
+		public static readonly int _VignetteColor = Shader.PropertyToID("_VignetteColor");
+	}
+
+	public void Start()
+	{
+		targetMaterial = GetComponent<Renderer>().sharedMaterial;
+	}
+
+	public void Update()
+	{
+		float squareVignetteBeginRaduis = vignetteBeginRadius * vignetteBeginRadius;
+		float vignetteRadii = vignetteBeginRadius + vignetteExpandRadius;
+		float oneOverVignetteRadiusDistance = 1f / (vignetteRadii - squareVignetteBeginRaduis);
+
+		targetMaterial.SetVector(Uniforms._VignetteShape, new Vector4(
+			4f * oneOverVignetteRadiusDistance * oneOverVignetteRadiusDistance,
+			-oneOverVignetteRadiusDistance * squareVignetteBeginRaduis));
+
+		// Premultiplying Alpha of vignette color
+		targetMaterial.SetColor(Uniforms._VignetteColor, new Color(
+				vignetteColor.r * vignetteColor.a,
+				vignetteColor.g * vignetteColor.a,
+				vignetteColor.b * vignetteColor.a,
+				vignetteColor.a));
+	}
+}
---
+++ Assets/Jcweeden/Editor/GenerateScreenSpaceMesh.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class GenerateScreenSpaceMesh 
+{
+    [MenuItem("Assets/Create/Screen space mesh")]
+    public static void CreateScreeSpaceMesh()
+    {
+        var mesh = new Mesh();
+        var verts = new Vector3[4];
+        var tris = new int[6];
+        var uvs = new Vector2[4];
+
+        // We will use these raw screenspace coordinates without additional projection matrices
+        verts[0] = new Vector3(-1f, -1f, 1f);
+        verts[1] = new Vector3(-1f, 1f, 1f);
+        verts[2] = new Vector3(1f, 1f, 1f);
+        verts[3] = new Vector3(1f, -1f, 1f);
+
+        tris[0] = 0;
+        tris[1] = 2;
+        tris[2] = 1;
+
+        tris[3] = 0;
+        tris[4] = 3;
+        tris[5] = 2;
+
+        uvs[0] = new Vector2(0f, 0f);    
+        uvs[1] = new Vector2(0f, 1f);    
+        uvs[2] = new Vector2(1f, 1f);    
+        uvs[3] = new Vector2(1f, 0f);    
+
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+
+        AssetDatabase.CreateAsset(mesh, "Assets/screenspaceMesh.asset");
+    }
+}
